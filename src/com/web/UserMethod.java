7aee7e8c7aaa3f5b60e75d17f81231db7f63f340
redgreencat <redgreencat65@yahoo.co.jp>
2017-06-24 18:59:17 +0800

    add comments

--- src/com/web/UserMethod.java
+++ src/com/web/UserMethod.java
@@ -19,6 +19,7 @@ public class UserMethod {
         String sql = "select * from users where username=?";
         List<Map<String, String>> list = DBUtil.query(sql, username);
         User user = null;
+        //查询用户是否存在，存在时list则会有数据
         if (list != null && !list.isEmpty()) {
             user = new User();
             try {
@@ -32,10 +33,17 @@ public class UserMethod {
         return user;
     }
 
+    /**
+     * 添加用户
+     * @param user User类
+     * @return boolean
+     */
     public boolean add(User user) {
         String sql = "insert into users(username, password, sex, birthday, hobby) values (?,?,?,?,?)";
         Object[] obj = {user.getUsername(), user.getPassword(), user.getSex(), user.getBirthday(), user.getHobby()};
+        //调用DBUtil方法，返回插入的ID
         Integer result = DBUtil.insert(sql, obj);
+        //判断ID是否大于0或不为NULL，大于0或不为NULL说明添加成功
         if (result != null && result > 0) {
             return true;
         }
